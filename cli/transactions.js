319a5ba04193bdbaeaaa1a40c52467cd851f740d
Álvaro Lillo Igualada <alilloig@gmail.com>
2025-05-23 11:03:58 +0200

    variables need to have the same name you know

--- cli/transactions.js
+++ cli/transactions.js
@@ -8,31 +8,36 @@ dotenv.config();
 const GAME_ADMIN_SECRET_KEY = process.env.GAME_ADMIN_SECRET_KEY;
 const GAME_ADMIN_ADDRESS = process.env.GAME_ADMIN_ADDRESS;
 const TRADE_WARS_ID = process.env.TRADE_WARS_ID;
-const GAME_ADMIN_CAPABILITY_ID = process.env.GAME_ADMIN_CAPABILITY_ID;
-const ERBIUM_TREASURY_ID = process.env.ERBIUM_TREASURY_ID;
-const LANTHANUM_TREASURY_ID = process.env.LANTHANUM_TREASURY_ID;
-const THORIUM_TREASURY_ID = process.env.THORIUM_TREASURY_ID;
+const ADM_CAP_ID = process.env.ADM_CAP_ID;
+const ERB_CAP_ID = process.env.ERB_CAP_ID;
+const LAN_CAP_ID = process.env.LAN_CAP_ID;
+const THO_CAP_ID = process.env.THO_CAP_ID;
 
 // Keypair from an existing secret key (Uint8Array)
 const keypair = Ed25519Keypair.fromSecretKey(GAME_ADMIN_SECRET_KEY);
+
+// create a new SuiClient object pointing to the network you want to use
+const client = new SuiClient({ url: getFullnodeUrl('devnet') });
 
 // Create element sources transaction
 const sources_tx = new Transaction();
 // Construct the method name string
 let createElementSourcesCall = TRADE_WARS_ID+'::trade_wars::create_element_sources';
+
 // Add a moveCall to the transaction
 sources_tx.moveCall({
 	target: createElementSourcesCall,
 	arguments: [
         sources_tx.object(TRADE_WARS_ID),
-        sources_tx.object(GAME_ADMIN_CAPABILITY_ID), 
-        sources_tx.object(ERBIUM_TREASURY_ID), 
-        sources_tx.object(LANTHANUM_TREASURY_ID), 
-        sources_tx.object(THORIUM_TREASURY_ID)],
+        sources_tx.object(ADM_CAP_ID), 
+        sources_tx.object(ERB_CAP_ID), 
+        sources_tx.object(LAN_CAP_ID), 
+        sources_tx.object(THO_CAP_ID)],
 });
+
 // Sign and execute the transaction
 // const [erb_source_id, lan_source_id, tho_source_id]
-export const sources = await client.signAndExecuteTransactionBlock({
+export const sources = await client.signAndExecuteTransaction({
 	transaction: sources_tx,
     signer: keypair,
 	requestType: 'WaitForLocalExecution',
@@ -51,7 +56,7 @@ start_universe_tx.moveCall({
 	target: startUniverseCall,
 	arguments: [
         start_universe_tx.object(TRADE_WARS_ID),
-        start_universe_tx.object(GAME_ADMIN_CAPABILITY_ID),
+        start_universe_tx.object(ADM_CAP_ID),
         start_universe_tx.object(sources[0]),
         start_universe_tx.object(sources[1]),
         start_universe_tx.object(sources[2]),
@@ -63,7 +68,7 @@ start_universe_tx.moveCall({
     ],
 });
 // Sign and execute the transaction
-export const universe = await client.signAndExecuteTransactionBlock({
+export const universe = await client.signAndExecuteTransaction({
 	transaction: start_universe_tx,
     signer: keypair,
 	requestType: 'WaitForLocalExecution',
