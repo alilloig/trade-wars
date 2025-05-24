f4fa50328f9460154882a138a8532f4773d0c318
Álvaro Lillo Igualada <alilloig@gmail.com>
2025-05-24 03:34:25 +0200

    font

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -126,7 +126,6 @@ function App() {
           <Heading 
             style={{ 
               cursor: 'pointer',
-              fontFamily: 'Orbitron, sans-serif',
               fontWeight: '700',
               fontOpticalSizing: 'auto',
               margin: 0,
